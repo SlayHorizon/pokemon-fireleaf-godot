5e2d923eaaf2df848f9864ce2672164002d05afb
nerfis-code <nerfisalcantara2017@gmail.com>
2025-05-08 09:48:02 -0400

    added player chacter movement and collision

--- game/source/overworld/player.gd
+++ game/source/overworld/player.gd
@@ -1,44 +1,62 @@
 class_name Player
 extends CharacterBody2D
 
+@export var map: Node2D
+@onready var ground: TileMapLayer = map.get_child(0)
+@onready var upper_ground: TileMapLayer = map.get_child(1)
 
 const TILE_SIZE: int = 16
 
-var speed: float = 120.0
+var movement_delay: float = 0.2 # speed
 
 var movement_tween: Tween
-var input_vector: Vector2
 var is_moving: bool = false
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
+var looking_towards: Vector2 = Vector2.ZERO
+
 func _ready() -> void:
-	position = position.snappedf(TILE_SIZE / 2)
+	global_position = _fix_position(global_position)
+
+func _fix_position(vec: Vector2):
+	return ground.local_to_map(vec) * Vector2i(TILE_SIZE, TILE_SIZE)
 
 
-#func _unhandled_key_input(_event: InputEvent) -> void:
-func _physics_process(delta: float) -> void:
-	input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+func _physics_process(_delta: float) -> void:
+	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+	if input_vector == Vector2.ZERO and not is_moving:
+		set_animation(looking_towards)
+
+	var horizontal = input_vector.x
+	var vertical = input_vector.y
+
 	if input_vector and not is_moving:
-		try_to_move()
+		try_to_move(Vector2(horizontal, 0) if horizontal else Vector2(0, vertical))
 
-
-func try_to_move() -> void:
+func try_to_move(input_vector: Vector2) -> void:
+	if input_vector != looking_towards:
+		looking_towards = input_vector
+		set_animation(looking_towards)
+		return
+	
+	var no_fixed_target_position: Vector2 = position + input_vector * TILE_SIZE
+	var tile_local := ground.local_to_map(no_fixed_target_position)
+	#TODO handler when chacter exit
+	var map_exit = ground.get_cell_tile_data(tile_local) == null
+	var collision = upper_ground.get_cell_tile_data(tile_local) != null
+	if (collision): return
+	
 	is_moving = true
-	#if movement_tween and movement_tween.is_valid():
-		#return
-	var target_position: Vector2 = (position + input_vector * TILE_SIZE).snappedf(TILE_SIZE)
+	var fixed_target_position: Vector2 = tile_local * Vector2i(TILE_SIZE, TILE_SIZE)
+	print(fixed_target_position)
+	
 	set_animation(input_vector, "_walk")
 	movement_tween = create_tween()
-	movement_tween.tween_property(self, "position", target_position, 0.4)
-	movement_tween.finished.connect(foo.bind(input_vector))
-
-
-func foo(direction: Vector2) -> void:
-	is_moving = false
-	if not input_vector:
-		set_animation(direction)
+	movement_tween.tween_property(self, "position", fixed_target_position, movement_delay)
+	movement_tween.finished.connect(func(): is_moving = false)
 
 
 func set_animation(direction: Vector2, state: String = "") -> void:
